6ef513cb0c270362c4346834bb097a39bd539eef
cv-dote <makoto.zaima.p4@tohoku.ac.jp>
2021-03-24 18:53:25 +0900

    Create Log In Screen and Sign Up Screen

--- App.jsx
+++ App.jsx
@@ -4,9 +4,11 @@ import React from 'react';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 //import MemoCreateScreen from './src/screens/MemoCreateScreen';
 //import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   return (
-    <MemoEditScreen />
+    <SignUpScreen />
   );
 }
